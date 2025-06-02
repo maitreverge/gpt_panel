fb33005f3017c25715eeb3dea145e5209d8038cd
“maitreverge_42” <verge.f@gmail.com>
2025-06-02 15:53:54 +0200

    Hardcoded buttons testing

--- gpt_panel/App.py
+++ gpt_panel/App.py
@@ -4,6 +4,27 @@ import buttons
 WINDOWS_WIDTH = 1200
 WINDOWS_HEIGHT = 1900
 
+class MyCheckboxFrame(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.checkbox_1 = customtkinter.CTkCheckBox(self, text="checkbox 1")
+        self.checkbox_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox 2")
+        self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.checkbox_3 = customtkinter.CTkCheckBox(self, text="checkbox 3")
+        self.checkbox_3.grid(row=2, column=0, padx=10, pady=(10, 0), sticky="w")
+    
+    def get(self):
+        checked_checkboxes = []
+        if self.checkbox_1.get() == 1:
+            checked_checkboxes.append(self.checkbox_1.cget("text"))
+        if self.checkbox_2.get() == 1:
+            checked_checkboxes.append(self.checkbox_2.cget("text"))
+        if self.checkbox_3.get() == 1:
+            checked_checkboxes.append(self.checkbox_3.cget("text"))
+        return checked_checkboxes
+
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -11,16 +32,16 @@ class App(customtkinter.CTk):
 
         self.title("my app")
         self.geometry(f"{WINDOWS_HEIGHT}x{WINDOWS_WIDTH}")
-        self.grid_columnconfigure((0, 1), weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
-        self.button = customtkinter.CTkButton(
-            self, text="my button", command=self.button_callback
-        )
-        self.button.grid(row=0, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
-        self.checkbox_1 = customtkinter.CTkCheckBox(self, text="checkbox 1")
-        self.checkbox_1.grid(row=1, column=0, padx=20, pady=(0, 20), sticky="w")
-        self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox 2")
-        self.checkbox_2.grid(row=1, column=1, padx=20, pady=(0, 20), sticky="w")
+        self.checkbox_frame = MyCheckboxFrame(self)
+        self.checkbox_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsw")
+        # self.checkbox_frame2 = MyCheckboxFrame(self)
+        # self.checkbox_frame2.grid(row=0, column=1, padx=10, pady=(10, 0), sticky="nsw")
+
+        self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback)
+        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
 
     def button_callback(self):
-        print("button pressed")
+        print("button pressed", self.checkbox_frame.get())
