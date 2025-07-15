bccf534a2c367610b6cfd2a9a5902578eece6717
“maitreverge_42” <verge.f@gmail.com>
2025-07-15 15:49:08 +0200

    Better style + improved intermediate window behaviour

--- gpt_panel/Api_window.py
+++ gpt_panel/Api_window.py
@@ -6,6 +6,7 @@ from openai import OpenAI
 WINDOWS_HEIGHT = 800
 WINDOWS_WIDTH = 800
 
+
 class ToplevelWindow(ctk.CTkToplevel):
     def __init__(self, *args, txt_input="", **kwargs):
         super().__init__(*args, **kwargs)
@@ -14,6 +15,7 @@ class ToplevelWindow(ctk.CTkToplevel):
         self.label = ctk.CTkLabel(self, text=txt_input)
         self.label.pack(padx=20, pady=20)
         self.title("⛔  INVALID KEY  ⛔")
+
 
 class Api_window(ctk.CTk):
     def __init__(self):
@@ -31,7 +33,7 @@ class Api_window(ctk.CTk):
         self.env_path = "../.env"
 
         ################# ? USEFULL ??
-        self.api_key = "" 
+        self.api_key = ""
 
         self.welcome_text = """
         GPT PANEL\n
@@ -44,17 +46,14 @@ class Api_window(ctk.CTk):
             width=300,
             # border_color="#BE3030", # useless need reading doc
             # wrap="word",
-            justify="center"
+            justify="center",
         )
 
         self.main_text.grid(row=0, column=0, sticky="we")
 
         # self.main_text.insert(0.0, self.welcome_text)
 
-        self.api_prompt = ctk.CTkTextbox(
-            self,
-            state="normal"
-        )
+        self.api_prompt = tk.Entry(self, textvariable="testhehe", show="\u2022")
 
         self.api_prompt.grid(row=1, column=0, sticky="we")
 
@@ -62,8 +61,6 @@ class Api_window(ctk.CTk):
             self,
             text="DISPLAY API KEY",
             command=self.hide_key,
-            onvalue="on",
-            offvalue="off",
         )
 
         self.hide_checkbox.grid(row=2, column=0)
@@ -81,22 +78,28 @@ class Api_window(ctk.CTk):
             command=self.quit_program,
         )
         self.cancel_button.grid(row=3, column=1)
-    
+
     def quit_program(self, event=None):
         print(f"QUIT THE PROGRAM")
         sys.exit(1)
-    
+
     def hide_key(self, event=None):
         """
         Selector to switch between "*" and real API key.
         """
-        raise NotImplementedError
+        is_checked = self.hide_checkbox.get()
+
+        if is_checked:
+            self.api_prompt.configure(show="")
+        else:
+            self.api_prompt.configure(show="\u2022")
 
     def validate_key(self, event=None):
         """
         Returns True if the API key is valid, False is not
         """
-        self.api_key = self.api_prompt.get(1.0, tk.END).strip()
+        # self.api_key = self.api_prompt.get(1.0, tk.END).strip()
+        self.api_key = self.api_prompt.get().strip()
         client = OpenAI(api_key=self.api_key)
         try:
             client.models.list()
@@ -104,13 +107,12 @@ class Api_window(ctk.CTk):
             # !   Warn the user about his invalid key
             print(f"⛔ INCORRECT API KEY")
             self.toplevel_window = ToplevelWindow(
-                self,
-                txt_input="INVALID KEY, PLEASE ENTER A VALID KEY"
+                self, txt_input="INVALID KEY, PLEASE ENTER A VALID KEY"
             )
         else:
             # !  Write the key in the format OPEN_AI_KEY=... in the .env file
             self.write_key_env()
-    
+
     def write_key_env(self):
         """
         Write the API key at the root of the repo in the following format:
@@ -119,4 +121,5 @@ class Api_window(ctk.CTk):
         with open(self.env_path, "w") as f:
             target = f"OPEN_AI_KEY={self.api_key}"
             f.write(target)
-        # raise NotImplementedError
+        # ! quit the intermediate window and switch to the main one.
+        self.destroy()
--- gpt_panel/main.py
+++ gpt_panel/main.py
@@ -4,31 +4,34 @@ from Api_validator import Api_validator
 from Api_window import Api_window
 import sys
 
+
+def intermediate_api_check():
+    """
+    Opens un a Tkinter Window for the user to prompt his own OpenAI API key.
+    The window keeps openning when closed unless `CANCEL` button is pressed.
+    """
+    while True:
+        try:
+            starter = Api_validator()
+            starter.validate_key()
+            break
+        except Exception as e:
+            # ! Launch the API window
+            print(f"LAUNCHING WINDOW API. Error type : {e}")
+            api_w = Api_window()
+            api_w.mainloop()
+
+
 def main():
 
-    ########### Block for entering the api key
-    # while True:
-    try:
-        starter = Api_validator()
-        starter.validate_key()
-        # break # ! Encapsulate this later in a While True loop
-    except Exception as e:
-        # ! Launch the API window
-        print(f"LAUNCHING WINDOW API. Error type : {e}")
-        api_w = Api_window()
-        api_w.mainloop()
-            # sys.exit(1)
-    
-    ########### Block for entering the api key
-    
+    intermediate_api_check()
+
     app = App()
 
     print(app.gpt_engine)  # This the API key
 
     app.mainloop()
 
-    # Prints total spent within the execution
-    # ! .total_spent is the getter, ._total_spent is the actual variable
     print(app.gpt_engine.price_engine.total_spent)
 
 
